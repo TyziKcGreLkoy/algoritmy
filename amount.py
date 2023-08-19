8008c8977ff443481a7b2c582ba07304e53ae9d7
igors <igor.sofins@mail.ru>
2023-08-19 16:19:57 +0400

    lox

---
+++ amount.py
@@ -0,0 +1,11 @@
+from random_generation import random_generator
+def amount(num):
+    if num == []:
+        return 0
+    else:
+        return num[0] + amount(num[1:])
+
+
+numbers = random_generator(10, 0, 50)
+print(numbers)
+print(amount(numbers))
